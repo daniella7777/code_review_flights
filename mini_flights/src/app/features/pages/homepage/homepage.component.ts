f40eae1a1cb2e0eef2b3e77d5164dcb2710cb26c
Daniella7777 <daniellac.iisr.2004@gmail.com>
2025-01-21 17:54:02 +0200

    basic homepage works

--- mini_flights/src/app/features/pages/homepage/homepage.component.ts
+++ mini_flights/src/app/features/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FindFlightComponent } from '../../find-flight/find-flight.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FindFlightComponent],
+  imports: [CommonModule, FindFlightComponent],
   template: `
     <app-find-flight></app-find-flight>
   `
